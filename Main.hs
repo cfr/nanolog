624fdb9858b0aa95dba98dd269c6b19ca982f588
Stan Serebryakov <i@cfr.pw>
2018-10-19 13:37:11 +0300

    Trying TLS

--- Main.hs
+++ Main.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Web.Scotty
 
 import Data.Monoid (mconcat)
 import Control.Monad.IO.Class (liftIO)
+
 import qualified Data.Text.Lazy as L
 import qualified Data.Text.Lazy.IO as L
+
+import Web.Scotty
+import Web.Scotty.TLS
+
 
 writeLog :: L.Text -> L.Text -> IO ()
 writeLog name log = do
@@ -13,6 +17,7 @@ writeLog name log = do
     L.putStrLn $ mconcat ["Written to ", name, "."]
   
 
+--main = scottyTLS 8087 "tls/privkey.pem" "tls/fullchain.pem" $
 main = scotty 8087 $
     get "/log/:log:name" $ do
         log <- param "log"
