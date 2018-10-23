f2f1f1215368774a5fa73ceed06740f790ea5483
Stan Serebryakov <i@cfr.pw>
2018-10-23 15:50:33 +0300

    Added timestamp

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-
 import Data.Monoid (mconcat)
+import Data.Time (getZonedTime)
 import Control.Monad.IO.Class (liftIO)
 
 import qualified Data.Text.Lazy as L
@@ -13,8 +13,11 @@ import Web.Scotty.TLS
 
 writeLog :: L.Text -> L.Text -> IO ()
 writeLog name log = do
-    L.appendFile ("logs/" ++ L.unpack name) $ mconcat [log, "\n"]
+    time <- timeStr 
+    L.appendFile ("logs/" ++ L.unpack name) $ mconcat [time, ": ", log, "\n"]
     L.putStrLn $ mconcat ["Written to ", name, "."]
+  where timeStr :: IO L.Text
+        timeStr = fmap (L.pack . show) getZonedTime
   
 
 --main = scottyTLS 8087 "tls/privkey.pem" "tls/fullchain.pem" $
